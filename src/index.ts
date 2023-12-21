60ce2f681975e9be543ad0429ae20629361be67e
Rajendra Khope <research@iocare.in>
2023-12-21 15:33:38 +0530

    fix: ifCond

--- src/index.ts
+++ src/index.ts
@@ -20,6 +20,53 @@ export const generatePDF = async (
     const browser = await puppeteer.launch(launch_options);
     const page = await browser.newPage();
 
+    hbs.registerHelper('ifCond', function(
+      v1: any,
+      operator: any,
+      v2: any,
+      options: any
+    ) {
+      switch (operator) {
+        case '==':
+          return v1 == v2 ? options.fn(data) : options.inverse(data);
+        case '===':
+          return v1 === v2 ? options.fn(data) : options.inverse(data);
+        case '!=':
+          return v1 != v2 ? options.fn(data) : options.inverse(data);
+        case '!==':
+          return v1 !== v2 ? options.fn(data) : options.inverse(data);
+        case '<':
+          return v1 < v2 ? options.fn(data) : options.inverse(data);
+        case '<=':
+          return v1 <= v2 ? options.fn(data) : options.inverse(data);
+        case '>':
+          return v1 > v2 ? options.fn(data) : options.inverse(data);
+        case '>=':
+          return v1 >= v2 ? options.fn(data) : options.inverse(data);
+        case '&&':
+          return v1 && v2 ? options.fn(data) : options.inverse(data);
+        case '||':
+          return v1 || v2 ? options.fn(data) : options.inverse(data);
+        default:
+          return options.inverse(options);
+      }
+    });
+
+    hbs.registerHelper({
+      eq: (v1: any, v2: any) => v1 === v2,
+      ne: (v1: any, v2: any) => v1 !== v2,
+      lt: (v1: number, v2: number) => v1 < v2,
+      gt: (v1: number, v2: number) => v1 > v2,
+      lte: (v1: number, v2: number) => v1 <= v2,
+      gte: (v1: number, v2: number) => v1 >= v2,
+      and() {
+        return Array.prototype.every.call(arguments, Boolean);
+      },
+      or() {
+        return Array.prototype.slice.call(arguments, 0, -1).some(Boolean);
+      },
+    });
+
     const html = await fs.readFile(filePath, 'utf8');
     const content = hbs.compile(html)(data);
     await page.setContent(content);
